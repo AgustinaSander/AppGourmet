ab39f661a13f39aeff36f347a9dfc6bd04ea266d
Agustina Belen Sander <asander@HEXACTA.COM>
2022-06-15 12:37:42 -0300

    Change config

--- src/main/java/com/example/demo/ClassConfig.java
+++ src/main/java/com/example/demo/ClassConfig.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EntityScan("com.example.demo.domain")
-//VER REPOSITORIES
+@EnableJpaRepositories(basePackages = "com.example.demo.repositories")
 public class ClassConfig {
 
 }
